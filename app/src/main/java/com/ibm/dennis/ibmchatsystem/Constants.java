381e9debce3e3cfc8a40ec470df2f177af0f6160
dennismuturia <dmuturia2922@gmail.com>
2017-11-13 11:10:37 +0300

    Initial commit

---
+++ app/src/main/java/com/ibm/dennis/ibmchatsystem/Constants.java
@@ -0,0 +1,11 @@
+package com.ibm.dennis.ibmchatsystem;
+
+/**
+ * Created by dennis on 11/13/17.
+ */
+
+public class Constants {
+    public static final String IBMCHATWATSONUSERNAME = BuildConfig.IBMCHATWATSONUSERNAME;
+    public static final String IBMCHATWATSONPASSWORD = BuildConfig.IBMCHATWATSONPASSWORD;
+    public static final String IBMCHATWATSONWORKSPACE = BuildConfig.IBMCHATWATSONWORKSPACE;
+}
