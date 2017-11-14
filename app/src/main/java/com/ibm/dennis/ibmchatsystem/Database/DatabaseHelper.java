4467e43f05c08088471d213ae420ec9102320adf
dennismuturia <dmuturia2922@gmail.com>
2017-11-14 11:25:50 +0300

    I would like to incorporate some sqlite in my project. So this class will be used to connect the app and the database

---
+++ app/src/main/java/com/ibm/dennis/ibmchatsystem/Database/DatabaseHelper.java
@@ -0,0 +1,8 @@
+package com.ibm.dennis.ibmchatsystem.Database;
+
+/**
+ * Created by dennis on 11/14/17.
+ */
+
+public class DatabaseHelper {
+}
--- app/src/main/java/com/ibm/dennis/ibmchatsystem/ui/MainActivity.java
+++ app/src/main/java/com/ibm/dennis/ibmchatsystem/ui/MainActivity.java
@@ -2,12 +2,14 @@ package com.ibm.dennis.ibmchatsystem.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
 import com.firebase.ui.auth.AuthUI;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.ibm.dennis.ibmchatsystem.R;
 
 import java.util.Arrays;
@@ -15,11 +17,12 @@ import java.util.Arrays;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     @BindView(R.id.myEditText)EditText myEditText;
     @BindView(R.id.sendButton)ImageButton sendButton;
     @BindView(R.id.speakButton)ImageButton speakButton;
     private static final int RC_SIGN_IN = 123;
+    private FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,5 +49,22 @@ public class MainActivity extends AppCompatActivity {
                     Toast.LENGTH_LONG)
                     .show();
         }
+        displayMessages();
+        sendButton.setOnClickListener(this);
+        speakButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == sendButton){
+
+        }
+        if (view == speakButton){
+
+        }
+    }
+    public void displayMessages(){
+        String uid = user.getUid();
+
     }
 }
