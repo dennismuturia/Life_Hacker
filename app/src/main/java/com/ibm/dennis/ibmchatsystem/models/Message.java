27d84de08da3f09363d15e03fc4f1ecc06551646
dennismuturia <dmuturia2922@gmail.com>
2017-11-14 08:40:07 +0300

    Sert the getter methods that will get all data that will be stored in the model.

--- app/src/main/java/com/ibm/dennis/ibmchatsystem/models/Message.java
+++ app/src/main/java/com/ibm/dennis/ibmchatsystem/models/Message.java
@@ -5,4 +5,31 @@ package com.ibm.dennis.ibmchatsystem.models;
  */
 
 public class Message {
+    public String message;
+    public String myMessage;
+    public int myTime;
+    public String user;
+
+    public Message(String message, String myMessage, int myTime, String user){
+        this.message = message;
+        this.myMessage = myMessage;
+        this.myTime = myTime;
+        this.user = user;
+    }
+
+    public int getMyTime() {
+        return myTime;
+    }
+
+    public String getMyMessage() {
+        return myMessage;
+    }
+
+    public void setMyMessage(String myMessage) {
+        this.myMessage = myMessage;
+    }
+
+    public String getUser() {
+        return user;
+    }
 }
