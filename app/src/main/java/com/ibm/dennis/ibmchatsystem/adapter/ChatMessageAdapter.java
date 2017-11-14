13e1ff3647c197609dd45f5d8769b16e9d5404a2
dennismuturia <dmuturia2922@gmail.com>
2017-11-14 11:03:22 +0300

    I have created a new adapter that will get all the responses and also inflate the required Views to post all the messages

---
+++ app/src/main/java/com/ibm/dennis/ibmchatsystem/adapter/ChatMessageAdapter.java
@@ -0,0 +1,8 @@
+package com.ibm.dennis.ibmchatsystem.adapter;
+
+/**
+ * Created by dennis on 11/14/17.
+ */
+
+public class ChatMessageAdapter {
+}
--- app/src/main/java/com/ibm/dennis/ibmchatsystem/ui/MainActivity.java
+++ app/src/main/java/com/ibm/dennis/ibmchatsystem/ui/MainActivity.java
@@ -2,7 +2,6 @@ package com.ibm.dennis.ibmchatsystem.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.Toast;
